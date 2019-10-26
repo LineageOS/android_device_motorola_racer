6afae074d7b397c08a5b9d1703383434eee478fa
Quallenauge <Hamsi2k@freenet.de>
2019-10-26 22:45:27 +0200

    racer: fingerprint: Relabel modules to device specific modules
    Change-Id: Iec3c48987d3a3387d32144c285561e1f253fe370

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.racer"
 
 #include <android/log.h>
 #include <hidl/HidlSupport.h>
