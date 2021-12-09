a253c7cd95b3c68787e1144d5d6a2a64a74565fa
hh2333 <tongsj2333@gmail.com>
2021-12-09 20:26:35 +0000

    racer: Don't call configureRpcThreadpool for camera postproc impl
     * In Android 12, no longer allowed to shrink the process pool after a
       larger process pool is started, we should patch camera postproc impl
       to nuke it from shrinking process pool and prevent triggering a fatal
       to cause the camera to crash.
       Ref: https://android.googlesource.com/platform/system/libhwbinder/+/8e047f7a626c782a02dc9e67dee2dbae35bc6956.
    
     * The following are the difference between before and after the
       patch.
       Before:
       MOV    W1, #1
       BL     ._ZN7android8hardware22configureRpcThreadpoolEmb
       Now:
       MOV    W1, #1
       NOP
    
     * Obtained by comparing the camera postproc impl modified by
       GitHub@erfanoabdi.
    
     * A tutorial on use hexdump, sed and xxd to patch hex in
       binary file: https://everydaywithlinux.blogspot.com/2012/11/patch-strings-in-binary-files-with-sed.html.
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I66c257e80bf56a48c640efc3c1c8949852edf756

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+    # Patch configureRpcThreadpool
+    vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+        hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8A0A0094/1F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+        mv "${TMPDIR}/${1##*/}" "${2}"
+        ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
