794648a197d12dcf5d5efab9bc1d4c945bb75a84
electimon <electimon@gmail.com>
2022-04-21 08:11:25 +0000

    racer: Rename camera.mot.is.coming.cts
    * This prop is illegal due to violating the
    * vendor property namespace rules
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I2da4951e19e97e5291cbb0f0b18cdae14f63fbaa

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,8 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib64/hw/com.qti.chi.override.so', 'vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/com.qti.feature2.gs.so'): blob_fixup()
+        .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
     'vendor/lib64/libvidhance.so': blob_fixup()
         .add_needed('libcomparetf2_shim.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
