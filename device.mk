c851e6404bb38a1263140f2c68f8a21819f8442d
Andrew Hexen <SyberHexen@gmail.com>
2022-09-16 03:28:54 -0700

    racer: device.mk: drop a2dp_in_audio_policy_configuration.xml
    * Already provided by common.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I2d451cf2a4df2275b5f9a20817ae1c1fd7a693b3

--- device.mk
+++ device.mk
@@ -39,7 +39,6 @@ PRODUCT_PACKAGES += \
 # Audio
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
