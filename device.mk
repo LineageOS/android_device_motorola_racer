6f411c85533c4bf3b75b2fcca8d93449ecd652c5
Nolen Johnson <johnsonnolen@gmail.com>
2023-01-27 22:37:46 -0500

    racer: Migrate to OSS fingerprint interface
    Change-Id: Ib179df9f9e509834eed2031d370d18ee98feac48

--- device.mk
+++ device.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.racer
+    android.hardware.biometrics.fingerprint@2.3-service.racer \
+    com.motorola.hardware.biometric.fingerprint@1.0.vendor
 
 # FM
 PRODUCT_PACKAGES += \
