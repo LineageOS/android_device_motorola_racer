adb640f9727fcb4303fde36618a21ce56d03d442
Bruno Martins <bgcngm@gmail.com>
2025-02-10 12:05:05 +0000

    racer: Replace BOARD_HAS_QCA_FM_SOC flag with sysprop
    Bluetooth HAL sets vendor.qcom.bluetooth.soc to `cherokee`, but FM2 app
    still reads persist.vendor.qcom.bluetooth.soc.
    
    Change-Id: Ic3ac5d5f488f83f620e3d0028f2c340d13fce312

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ TARGET_USES_FOD_ZPOS := true
 
 # FM
 BOARD_HAVE_QCOM_FM := true
-BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
