17cb5575fe9c1983cffdb108e06c6a824235a785
Michael Bestas <mkbestas@lineageos.org>
2025-02-23 03:00:52 +0200

    racer: Eliminate system.prop
    Change-Id: Ia05f96ee124b6164f6b6d7d9b29622dd9abfbba5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,6 @@ BOARD_DTBOIMG_PARTITION_SIZE := 2097152
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Copy to recovery
